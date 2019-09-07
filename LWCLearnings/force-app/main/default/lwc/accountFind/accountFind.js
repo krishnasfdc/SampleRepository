55210ec6d2db7cf80b393627c4ad67680b2ec48c
sforcebitbucket <jaykrishna.sforce@gmail.com>
2019-09-08 00:33:30 +0530

    AccountFind Changes commit

--- LWCLearnings/force-app/main/default/lwc/accountFind/accountFind.js
+++ LWCLearnings/force-app/main/default/lwc/accountFind/accountFind.js
@@ -1,3 +1,12 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 
-export default class AccountFind extends LightningElement {}
+export default class AccountFind extends LightningElement {
+
+    @api accName;
+
+    handleChange(event) {
+
+        /* eslint-disable no-console */
+        console.log(event.target.value);
+    }
+}

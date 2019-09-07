f793b06bcfa2cfd5363ed6f3f9d36b1645a4901e
sforcebitbucket <jaykrishna.sforce@gmail.com>
2019-09-08 01:17:20 +0530

    FindAccount Changes committed

--- LWCLearnings/force-app/main/default/lwc/accountFind/accountFind.js
+++ LWCLearnings/force-app/main/default/lwc/accountFind/accountFind.js
@@ -1,8 +1,16 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api, wire } from 'lwc';
+import findAllAccounts from '@salesforce/apex/LWC_AccountController.findAllAccounts';
 
 export default class AccountFind extends LightningElement {
 
     @api accName;
+
+    @wire(findAllAccounts) allAccounts;
+
+    get listIsNotEmpty() {
+
+        return this.allAccounts && Array.isArray(this.allAccounts.data) && this.allAccounts.data.length > 0;
+    }
 
     handleChange(event) {
 
